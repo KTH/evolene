436543b40500b6a6520fe44215da47bedcf862b3
Patric Jansson <paddy@kth.se>
2020-12-08 17:06:36 +0100

    docs

--- modules/pipeline_steps/tag_image_step.py
+++ modules/pipeline_steps/tag_image_step.py
@@ -4,6 +4,7 @@ from modules.pipeline_steps.abstract_pipeline_step import AbstractPipelineStep
 from modules.util import pipeline_data
 from modules.util import environment
 from modules.util import image_version_util
+from modules.util import artifact
 
 class TagImageStep(AbstractPipelineStep):
 
@@ -16,9 +17,16 @@ class TagImageStep(AbstractPipelineStep):
     def run_step(self, data): #pragma: no cover
         if pipeline_data.IMAGE_TAGS not in data:
             data[pipeline_data.IMAGE_TAGS] = []
+        
+        # Default tagging appname:1.2.3_abcdefg
         self.tag(image_version_util.get_image(data), data)
+        
+        # Default tagging appname:1.2.3
         self.tag(image_version_util.get_image_only_semver(data), data)
+        
+        # Default tagging appname:latest
         self.tag(image_version_util.get_latest_tag(data), data)
+        
         return data
 
     def tag(self, tag, data): #pragma: no cover
