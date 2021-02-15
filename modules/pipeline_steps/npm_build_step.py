f04f854adaa03bc8eb48f739630c46534ea40c09
Patric Jansson <paddy@kth.se>
2021-02-15 16:36:32 +0100

    Link to Jenkins in NPM error

--- modules/pipeline_steps/npm_build_step.py
+++ modules/pipeline_steps/npm_build_step.py
@@ -21,8 +21,13 @@ class NpmBuildStep(AbstractPipelineStep):
             result = nvm.run_npm_script(data, 'build')
         except PipelineException as npm_ex:
             self.handle_step_error(
-                'npm build failed',
-                npm_ex
-            )
+                    f'\n:rotating_light: <!here> *NPM build failed*, see <{environment.get_console_url()}|:jenkins: Jenkins console log here>.',
+                    self.get_stack_trace_shortend(npm_ex),
+                )
+
         self.log.debug('Output from npm build was: "%s"', result)
         return data
+
+
+    def get_stack_trace_shortend(self, exception):
+        return str(exception).replace('`', ' ')[-1000:]
