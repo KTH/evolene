97068e1a99e21315c4162f8c5e52d22758cac5a8
Patric Jansson <paddy@kth.se>
2021-04-27 22:44:32 +0200

    print version to users

--- run_github_action.sh
+++ run_github_action.sh
@@ -1,5 +1,12 @@
+EVOLENE_DIRECTORY=/repo
 #!/bin/sh
 
+# Print the version
+cat $EVOLENE_DIRECTORY/docker.conf
+
+# CD into the repository directory containing the source code to build (not Evolene).
 cd /src
+
+# Run Evolene on the repository code.
 EVOLENE_DIRECTORY=$EVOLENE_DIRECTORY PIPENV_PIPFILE=$EVOLENE_DIRECTORY/Pipfile \
     pipenv run python $EVOLENE_DIRECTORY/run.py
