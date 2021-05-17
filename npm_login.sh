57b66319c90ae3484d5bfff38c8c582b63b59235
Patric Jansson <paddy@kth.se>
2021-05-17 13:50:15 +0200

    Testing env passing

--- npm_login.sh
+++ npm_login.sh
@@ -1,17 +1,18 @@
-/usr/bin/expect <<EOD
-spawn npm login
-expect {
-  "Username:" {
-    send "$NPM_USERNAME\r";
-    exp_continue
-  }
-  "Password:" {
-    send "$NPM_PASSWORD\r";
-    exp_continue
-  }
-  "Email: (this IS public)" {
-    send "$NPM_EMAIL\r";
-    exp_continue
-  }
-}
-EOD
+echo "$NPM_USERNAME\r $NPM_EMAIL\r"
+# /usr/bin/expect <<EOD
+# spawn npm login
+# expect {
+#   "Username:" {
+#     send "$NPM_USERNAME\r";
+#     exp_continue
+#   }
+#   "Password:" {
+#     send "$NPM_PASSWORD\r";
+#     exp_continue
+#   }
+#   "Email: (this IS public)" {
+#     send "$NPM_EMAIL\r";
+#     exp_continue
+#   }
+# }
+# EOD
