efc34a4aced56679cd98b3f8b854f364a916d34f
Patric Jansson <paddy@kth.se>
2021-05-19 15:19:32 +0200

    Disable DryRun

--- modules/pipelines/docker_deploy_pipeline.py
+++ modules/pipelines/docker_deploy_pipeline.py
@@ -63,7 +63,7 @@ class DockerDeployPipeline(object):
             # It never to late to party
             CelebrateStep(),
             # Test run the image
-            DryRunStep(),
+            #DryRunStep(),
             # Run unit tests
             UnitTestStep(),
             # Run integration tests
