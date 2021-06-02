3f02d141284e0ff11e83b0a7db61266c00e4fe97
Patric Jansson <paddy@kth.se>
2021-06-02 21:54:29 +0200

    Log error

--- modules/util/process.py
+++ modules/util/process.py
@@ -6,7 +6,7 @@ import sys
 from modules.util.exceptions import PipelineException
 
 def run_with_output(cmd, log_cmd=False, check=False):
-    log = logging.getLogger(__name__)
+    log = logging.getLogger("-")
     try:
         if log_cmd:
             log.info("Command: '%s'", cmd)
@@ -23,6 +23,7 @@ def run_with_output(cmd, log_cmd=False, check=False):
                 return result.stderr
             
     except subprocess.CalledProcessError as cpe:
+        log.info(str(cpe))
         raise PipelineException(f'{cpe.output}')
     except:
         raise PipelineException(f'Unhandled exception when execution command.')
