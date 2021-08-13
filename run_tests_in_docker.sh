3a355cd914ffc582cdf7112cb7501ba24e0df21b
Patric Jansson <paddy@kth.se>
2021-08-13 11:37:11 +0200

    Run unit tests in Evolene

--- run_tests_in_docker.sh
+++ run_tests_in_docker.sh
@@ -5,7 +5,7 @@ echo "Running unit tests inside 🐳 Docker Compose"
 ID=$(docker build -q .) && \
     WORKSPACE=$(pwd) \LOCAL_IMAGE_ID=$ID \
     docker-compose \
-        --file docker-compose-unit-tests-new.yml up \
+        --file docker-compose-unit-tests.yml up \
         --force-recreate \
         --abort-on-container-exit \
         --always-recreate-deps \
