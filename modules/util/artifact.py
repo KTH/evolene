72a7e9ed04fa8adb2594dfbcb88c2ef9f0fc8f0b
Patric Jansson <paddy@kth.se>
2021-08-13 19:15:21 +0200

    text change

--- modules/util/artifact.py
+++ modules/util/artifact.py
@@ -6,32 +6,33 @@ from modules.util import git
 
 log = logging.getLogger("-")
 
+
 def branch_starts_with(pattern):
     if not pattern:
-        log.info(f'No pattern passed.')
+        log.debug('No pattern passed.')
         return False
 
     result = str(environment.get_git_branch()).startswith(pattern)
-        
-    log.info(f'Branch starts with {pattern}.')
+
+    log.debug(f'Branch starts with {pattern}.')
 
     return result
 
-def should_store():
 
-    
+def should_store():
     '''
     Should the artifact (npm packqge, docker image...) built be store in some repository somewhere?
     '''
     if git.is_main_branch():
-        log.info(f'Branch {git.is_main_branch()} will be stored.')
+        log.info(
+            f'Build artefact will be stored in repository since it is {environment.get_git_branch()}.')
         return True
 
     if branch_starts_with(environment.get_branches_save_starting_with()):
         log.info(f'Branch {environment.get_git_branch()} is not main/master but is configured to be stored anyway, according to BRANCHES_SAVE_STARTING_WITH: {environment.get_branches_save_starting_with()}')
         return True
 
-    log.info('Build will not be stored in any repository.')
-    
+    log.info(
+        f'Built artefact will not be stored in any repository, it is in a branch {environment.get_git_branch()}.')
+
     return False
-    
