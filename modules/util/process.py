9f96865c2d4c3d39574b863d46df57c07f36c337
Mikael Bengtsson <mibengt@kth.se>
2021-11-16 16:15:17 +0100

    Set BASH_ENV to init file

--- modules/util/process.py
+++ modules/util/process.py
@@ -2,7 +2,7 @@ __author__ = 'tinglev'
 
 import subprocess
 import logging
-import sys
+import os
 from modules.util.exceptions import PipelineException
 
 def run_with_output(cmd, log_cmd=False, check=False):
@@ -11,6 +11,7 @@ def run_with_output(cmd, log_cmd=False, check=False):
         if log_cmd:
             log.info("Command: '%s'", cmd)
 
+        os.environ["BASH_ENV"] = "~/.bashrc"
         result = subprocess.run(args = cmd,
                     shell=True,
                     executable="/bin/bash",
