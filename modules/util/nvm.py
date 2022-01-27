f8f1b7666d8083421959e8693f0eccf0070877ae
Mikael Bengtsson <mibengt@kth.se>
2022-01-27 15:21:43 +0100

    Rollback prod

--- modules/util/nvm.py
+++ modules/util/nvm.py
@@ -24,7 +24,7 @@ def get_npm_base(data):
     nvm_base = get_nvm_exec_base(data)
     project_path = environment.get_project_root()
     return (
-        f'{nvm_base} npm '
+        f'{nvm_base} npm --prefix {project_path}'
     )
 
 def run_npm_script(data, script_name):
