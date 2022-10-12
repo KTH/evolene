feaec14a9514edd6afc83dd2e7f5a6ebc2a4a190
Sebastian Ware <sebastian@urbantalk.se>
2022-10-12 11:41:00 +0200

    Give feedback on injected packages

--- modules/pipeline_steps/inject_npm_workspace_packages.py
+++ modules/pipeline_steps/inject_npm_workspace_packages.py
@@ -47,6 +47,7 @@ class InjectNpmWorkspacePackages(AbstractPipelineStep):
             # Copy local packages
             for package_name, pkg in local_packages.items():
                 # Need to add leading slash to source path
+                self.log.info(f'Injecting package "{package_name}" from "{pkg["path"]}"')
                 src = file_util.get_absolue_path(f"/{pkg['path']}", from_repos_root=True)
                 dest = file_util.get_absolue_path(f'/evolene_local_packages/{package_name}')
                 shutil.copytree(src, dest, dirs_exist_ok=True)
